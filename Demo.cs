086a4b4c5574a9c482eb432d9e75ee4b8687f13a
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-02-18 14:48:34 +0530

    Add files via upload

---
+++ Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOfNumber
+{
+    public class Demo
+    {
+        public int Add(int numOne, int numTwo)
+        {
+            int sum = numOne + numTwo;
+            return sum;
+        }
+    }
+}
